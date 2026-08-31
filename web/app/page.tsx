a5bcfaf5dc74da8b3861db6d6556fb603c0805a5
Rodrigo <cskadys@gmail.com>
2026-08-30 22:51:37 -0300

    fix: render client app from server route safely

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,3 +1,2 @@
-import dynamic from "next/dynamic";
-const AlphaBuildersApp = dynamic(() => import("@/components/AlphaBuildersApp"), { ssr: false });
+import AlphaBuildersApp from "@/components/AlphaBuildersApp";
 export default function Home(){ return <AlphaBuildersApp/>; }
